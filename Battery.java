5a0a4dc0169361b8d5364ca6644f0222150f3bad
Aliaksei Klimenka <Aliaksei_Klimenka1@epam.com>
2018-09-11 13:21:06 +0300

    Update Battery.java

--- Battery.java
+++ Battery.java
@@ -1,7 +1,7 @@
 package com.epam.mentoring.devices;
 
 public interface Battery {
-//
+//TODO: AK Не совсем понятна идея этого интерфейса, почему метод статический и зачем мы передаем в него batteryCharge?
     public static int findBatteryCharge(int batteryCharge) {
         return batteryCharge;
     }
